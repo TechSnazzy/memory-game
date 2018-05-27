07e60271f23f50de225529301d032aa9d2f38ba0
Sean Morrison <sean1972@gmail.com>
2018-05-27 15:04:14 -0700

    Created comments to make it easier to distinguish between snippets of code

--- js/app.js
+++ js/app.js
@@ -1,15 +1,20 @@
 /*
- * Create a list that holds all of your cards
+ * Create a List of Cards
  */
-
 const symbols = ['fa fa-diamond', 'fa fa-diamond', 'fa fa-paper-plane-o', 'fa fa-paper-plane-o', 'fa fa-anchor', 'fa fa-anchor', 'fa fa-bolt', 'fa fa-bolt', 'fa fa-cube', 'fa fa-cube', 'fa fa-leaf', 'fa fa-leaf', 'fa fa-bicycle', 'fa fa-bicycle', 'fa fa-bomb', 'fa fa-bomb'];
 
+/*
+ * Set Application Variables
+ */
 const deck = document.querySelector('.deck');
 const timer = document.querySelector('.timer');
 let cards = [];
 let match = [];
 let currentTimer;
 
+/*
+ * Start a New Game
+ */
 function startGame() {
   shuffle(symbols);
   for (let i=0; i<symbols.length; i++) {
@@ -27,19 +32,20 @@ function startGame() {
   }
 }
 
+/*
+ * Make the Card Clickable
+ */
 function click(card) {
   card.addEventListener('click', function() {
 
     const current = this;
     const previous = cards[0];
 
+    // when clicked, push open/show classes to card
     if(cards.length === 1) {
-      // when clicked, push open/show classes to card
       card.classList.add('open', 'show', 'disable');
       cards.push(this);
-
       compare(current, previous);
-
     } else {
       card.classList.add('open', 'show', 'disable');
       cards.push(this);
@@ -47,35 +53,32 @@ function click(card) {
   });
 }
 
-// compare 2 cards
+/*
+ * Compare 2 Cards
+ */
 function compare(current, previous) {
-
-  // matcher
+  // Add the Match Class
   if(current.innerHTML === previous.innerHTML) {
     current.classList.add('match');
     previous.classList.add('match');
-
     match.push(current, previous);
-
     cards = [];
-
     gameOver(); // check if the game is over
-
   } else {
-
     cards = [];
-
     // set a 500ms delay for closing cards
     setTimeout(function() {
       current.classList.remove('open', 'show', 'disable');
       previous.classList.remove('open', 'show', 'disable');
     }, 500);
   }
-
   // add new move
   addMove();
 }
 
+/*
+ * Game Over
+ */
 function gameOver() {
   if (match.length === symbols.length) {
     stopTimer(currentTimer);
@@ -87,7 +90,9 @@ function gameOver() {
   }
 }
 
-// add a move
+/*
+ * Increment Move Counter
+ */
 const gameMoves = document.querySelector('.moves');
 let moves = 0;
 gameMoves.innerHTML = 'Moves: ' + moves;
@@ -95,11 +100,12 @@ gameMoves.innerHTML = 'Moves: ' + moves;
 function addMove() {
   moves++;
   gameMoves.innerHTML = 'Moves: ' + moves; // add it to the container
-
   rating();
 }
 
-// rating system
+/*
+ * Star Rating System
+ */
 const gameStars = document.querySelector('.stars');
 gameStars.innerHTML = `<li><i class="fa fa-star"></i></li><li><i class="fa fa-star"></i></li><li><i class="fa fa-star"></i></li>`;
 function rating() {
@@ -113,8 +119,9 @@ function rating() {
   }
 }
 
-
-// start the timer
+/*
+ * Start the Timer
+ */
 function startTimer() {
   currentTimer = setInterval(function() {
     timer.innerHTML = 'Time: ' + second;
@@ -122,14 +129,16 @@ function startTimer() {
   }, 1000);
 }
 
-// reset the timer
+/*
+ * Stop the Timer
+ */
 function stopTimer(timer) {
   clearInterval(timer);
 }
 
-
-
-// restart button
+/*
+ * Restart Button
+ */
 const restartButton = document.querySelector('.restart');
 restartButton.addEventListener('click', function() {
   deck.innerHTML = "";
@@ -140,9 +149,7 @@ restartButton.addEventListener('click', function() {
   gameStars.innerHTML = `<li><i class="fa fa-star"></i></li><li><i class="fa fa-star"></i></li><li><i class="fa fa-star"></i></li>`;
 });
 
-// start the game for the first time
-startGame();
-
+startGame(); // start the game for the first time
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
