23ba36bef4e7cf550487879becad08028dd0f289
Sean Morrison <sean1972@gmail.com>
2019-05-19 18:00:05 -0700

    Cleaned up the code with VSCode prettier extension

--- js/app.js
+++ js/app.js
@@ -1,7 +1,24 @@
 /*
  * Create a List of Cards
  */
-const symbols = ['fa fa-diamond', 'fa fa-diamond', 'fa fa-paper-plane-o', 'fa fa-paper-plane-o', 'fa fa-anchor', 'fa fa-anchor', 'fa fa-bolt', 'fa fa-bolt', 'fa fa-cube', 'fa fa-cube', 'fa fa-leaf', 'fa fa-leaf', 'fa fa-bicycle', 'fa fa-bicycle', 'fa fa-bomb', 'fa fa-bomb'];
+const symbols = [
+  'fa fa-diamond',
+  'fa fa-diamond',
+  'fa fa-paper-plane-o',
+  'fa fa-paper-plane-o',
+  'fa fa-anchor',
+  'fa fa-anchor',
+  'fa fa-bolt',
+  'fa fa-bolt',
+  'fa fa-cube',
+  'fa fa-cube',
+  'fa fa-leaf',
+  'fa fa-leaf',
+  'fa fa-bicycle',
+  'fa fa-bicycle',
+  'fa fa-bomb',
+  'fa fa-bomb'
+];
 
 /*
  * Set Application Variables
@@ -17,7 +34,7 @@ let currentTimer;
  */
 function startGame() {
   shuffle(symbols);
-  for (let i=0; i<symbols.length; i++) {
+  for (let i = 0; i < symbols.length; i++) {
     const card = document.createElement('li');
     const icon = symbols[i];
     const markup = `<i class="${icon}"></i>`;
@@ -26,9 +43,9 @@ function startGame() {
     deck.appendChild(card);
     click(card);
     stopTimer(currentTimer);
-  	second = 0;
+    second = 0;
     timer.innerHTML = 'Time: ' + second;
-  	startTimer();
+    startTimer();
   }
 }
 
@@ -37,12 +54,11 @@ function startGame() {
  */
 function click(card) {
   card.addEventListener('click', function() {
-
     const current = this;
     const previous = cards[0];
 
     // when clicked, push open/show classes to card
-    if(cards.length === 1) {
+    if (cards.length === 1) {
       card.classList.add('open', 'show', 'disable');
       cards.push(this);
       compare(current, previous);
@@ -58,7 +74,7 @@ function click(card) {
  */
 function compare(current, previous) {
   // Add the Match Class
-  if(current.innerHTML === previous.innerHTML) {
+  if (current.innerHTML === previous.innerHTML) {
     current.classList.add('match', 'animated', 'rubberBand');
     previous.classList.add('match', 'animated', 'rubberBand');
     match.push(current, previous);
@@ -85,8 +101,16 @@ function gameOver() {
     setTimeout(function() {
       let starLength = $('.fa-star').length;
       $('#exampleModal').modal('show');
-      $('.modal-body').html('You won the game in ' + moves + ' moves. It took you ' + second + ' seconds. You have a ' + starLength + ' star rating. Pretty awesome! Would you like to play again?');
-    }, 200)
+      $('.modal-body').html(
+        'You won the game in ' +
+          moves +
+          ' moves. It took you ' +
+          second +
+          ' seconds. You have a ' +
+          starLength +
+          ' star rating. Pretty awesome! Would you like to play again?'
+      );
+    }, 200);
   }
 }
 
@@ -109,13 +133,13 @@ function addMove() {
 const gameStars = document.querySelector('.stars');
 gameStars.innerHTML = `<li><i class="fa fa-star"></i></li><li><i class="fa fa-star"></i></li><li><i class="fa fa-star"></i></li>`;
 function rating() {
-  switch(moves) {
+  switch (moves) {
     case 15:
       gameStars.innerHTML = `<li><i class="fa fa-star animated jello"></i></li><li><i class="fa fa-star"></i></li>`;
-    break;
+      break;
     case 20:
       gameStars.innerHTML = `<li><i class="fa fa-star animated jello"></i></li>`;
-    break;
+      break;
   }
 }
 
@@ -141,7 +165,7 @@ function stopTimer(timer) {
  */
 const restartButton = document.querySelector('.restart');
 restartButton.addEventListener('click', function() {
-  deck.innerHTML = "";
+  deck.innerHTML = '';
   startGame();
   match = [];
   moves = 0;
@@ -153,15 +177,17 @@ startGame(); // start the game for the first time
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
-    var currentIndex = array.length, temporaryValue, randomIndex;
+  var currentIndex = array.length,
+    temporaryValue,
+    randomIndex;
 
-    while (currentIndex !== 0) {
-        randomIndex = Math.floor(Math.random() * currentIndex);
-        currentIndex -= 1;
-        temporaryValue = array[currentIndex];
-        array[currentIndex] = array[randomIndex];
-        array[randomIndex] = temporaryValue;
-    }
+  while (currentIndex !== 0) {
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
 
-    return array;
+  return array;
 }
